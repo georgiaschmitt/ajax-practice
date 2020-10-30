a84a991e97e9ce30cfd5638a434a7bb23a78950e
georgiaschmitt <georgia.schmitt@gmail.com>
2020-10-30 21:03:07 +0000

    Added serialized forms and selector.load() method
    Co-authored-by: jadele511 <baongocle1991@gmail.com>

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,10 +4,11 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
-    $.get('/fortune', (response) => {
-      $('#fortune-text').html(response);  
-    });
-}
+//     $.get('/fortune', (response) => {
+//       $('#fortune-text').html(response);  
+//     });
+    $('#fortune-text').load('/fortune')
+};
 
 $('#get-fortune-button').on('click', showFortune);
 
@@ -19,8 +20,9 @@ function showWeather(evt) {
     evt.preventDefault();
 
     let url = "/weather.json";
-    let formData = {"zipcode": $("#zipcode-field").val()};
-    // TODO: request weather with that URL and show the forecast in #weather-info
+    // let formData = {"zipcode": $("#zipcode-field").val()};
+    // // TODO: request weather with that URL and show the forecast in #weather-info
+    const formData = $('#weather-form').serialize()
 
     $.get(url, formData, (res) => {
         $("#weather-info").html(res.forecast)
@@ -38,16 +40,20 @@ function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
-    const formInputs = {
-        "melon_type": $("#melon-type-field").val(),
-        "qty": $("#qty-field").val()
-    };
-    $.post('/order-melons.json', formInputs, (res) => {
+    // const formInputs = {
+    //     "melon_type": $("#melon-type-field").val(),
+    //     "qty": $("#qty-field").val()
+    // };
+    const url = '/order-melons.json'
+    const formInputs = $('#order-form').serialize()
+
+    $.post(url, formInputs, (res) => {
         $('#order-status').html(res.msg);
         if (res.code === 'ERROR') {
             $('#order-status').addClass("order-error");
         }
-    })
+    });
+
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 };
 
