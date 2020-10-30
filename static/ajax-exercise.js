e70615120b2b93712b3b511d07ed1ef20ca2629b
georgiaschmitt <georgia.schmitt@gmail.com>
2020-10-30 20:07:33 +0000

    Finished orderMelons function
    Co-authored-by: jadele511 <baongocle1991@gmail.com>

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -38,8 +38,18 @@ function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
+    const formInputs = {
+        "melon_type": $("#melon-type-field").val(),
+        "qty": $("#qty-field").val()
+    };
+    $.post('/order-melons.json', formInputs, (res) => {
+        $('#order-status').html(res.msg);
+        if (res.code === 'ERROR') {
+            $('#order-status').addClass("order-error");
+        }
+    })
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
+};
 
 $("#order-form").on('submit', orderMelons);
 
